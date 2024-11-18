509512260c21fc6f7036196f83dc3b4d59af7b68
root <martin.weiss@suse.com>
2024-11-18 17:58:55 +0100

    adding combustion example and adjusting SSL for repos

--- build.sh
+++ build.sh
@@ -13,33 +13,25 @@ mkdir -p $TARGET_DIR/image
 # build the image
 
 #kiwi-ng --profile aarch64-self_install-gpu system build --target-dir $TARGET_DIR/image --description $TARGET_DIR \
-#--add-repo http://susemanager.weiss.ddnss.de/pub/isos/slmicro60a64 \
-#--add-repo http://susemanager.weiss.ddnss.de/ks/dist/slmicro60a64-test,repo-md,SL-Micro-6.0-Test-Pool \
-#--add-repo http://susemanager.weiss.ddnss.de/ks/dist/child/staging-slmicro60a64-test-sndirsch-sidecar-slmicro60-a64/slmicro60a64-test,repo-md,sndirsch-sidecar-slmicro60-a64 \
-#--add-repo http://susemanager.weiss.ddnss.de/ks/dist/child/staging-slmicro60a64-test-libnvidia-container-a64/slmicro60a64-test,repo-md,libnvidia-container-a64 \
-#--add-repo http://susemanager.weiss.ddnss.de/ks/dist/child/staging-slmicro60a64-test-sl-micro-extras-6.0-pool-aarch64/slmicro60a64-test \
-#--add-repo http://susemanager.weiss.ddnss.de/ks/dist/child/sl-micro-6.0-pool-aarch64-clone/slmicro60a64-test
-
+#--add-repo https://susemanager.weiss.ddnss.de/pub/isos/slmicro60a64 \
+#--add-repo https://susemanager.weiss.ddnss.de/ks/dist/slmicro60a64-test,repo-md,SL-Micro-6.0-Test-Pool \
+#--add-repo https://susemanager.weiss.ddnss.de/ks/dist/child/staging-slmicro60a64-test-sndirsch-sidecar-slmicro60-a64/slmicro60a64-test,repo-md,sndirsch-sidecar-slmicro60-a64 \
+#--add-repo https://susemanager.weiss.ddnss.de/ks/dist/child/staging-slmicro60a64-test-libnvidia-container-a64/slmicro60a64-test,repo-md,libnvidia-container-a64 \
+#--add-repo https://susemanager.weiss.ddnss.de/ks/dist/child/staging-slmicro60a64-test-sl-micro-extras-6.0-pool-aarch64/slmicro60a64-test \
+#--add-repo https://susemanager.weiss.ddnss.de/ks/dist/child/sl-micro-6.0-pool-aarch64-clone/slmicro60a64-test
 #exit
 
-#podman run --privileged -v $TARGET_DIR:/image:Z registry.suse.com/bci/kiwi:9.24.43-10.7 kiwi-ng --profile aarch64-self_install-gpu system build --description /image --target-dir /image/image \
-podman run --privileged -v $TARGET_DIR/kiwi.yml:/etc/kiwi.yml  -v $TARGET_DIR:/image:Z registry.suse.com/bci/kiwi:10.1.10 kiwi-ng --profile aarch64-self_install-gpu-encrypted system build --description /image --target-dir /image/image \
---add-repo http://susemanager.weiss.ddnss.de/pub/isos/slmicro60a64 \
---add-repo http://susemanager.weiss.ddnss.de/ks/dist/slmicro60a64-test,repo-md,SL-Micro-6.0-Test-Pool \
---add-repo http://susemanager.weiss.ddnss.de/ks/dist/child/staging-slmicro60a64-test-sndirsch-sidecar-slmicro60-a64/slmicro60a64-test,repo-md,sndirsch-sidecar-slmicro60-a64 \
---add-repo http://susemanager.weiss.ddnss.de/ks/dist/child/staging-slmicro60a64-test-libnvidia-container-a64/slmicro60a64-test,repo-md,libnvidia-container-a64 \
---add-repo http://susemanager.weiss.ddnss.de/ks/dist/child/staging-slmicro60a64-test-sl-micro-extras-6.0-pool-aarch64/slmicro60a64-test \
---add-repo http://susemanager.weiss.ddnss.de/ks/dist/child/sl-micro-6.0-pool-aarch64-clone/slmicro60a64-test
-
+podman run --privileged -v /var/lib/ca-certificates:/var/lib/ca-certificates -v /var/lib/Kiwi/repo:/var/lib/Kiwi/repo -v $TARGET_DIR/kiwi.yml:/etc/kiwi.yml  -v $TARGET_DIR:/image:Z registry.suse.com/bci/kiwi:10.1.10 kiwi-ng --debug --profile aarch64-self_install-gpu-encrypted system build --allow-existing-root --description /image --target-dir /image/image \
+--ignore-repos-used-for-build --add-repo file:/var/lib/Kiwi/repo,rpm-dir,common_repo,90,false,false --add-bootstrap-package findutils --add-bootstrap-package rhn-org-trusted-ssl-cert-osimage \
+--add-repo https://susemanager.weiss.ddnss.de/pub/isos/slmicro60a64 \
+--add-repo https://susemanager.weiss.ddnss.de/ks/dist/slmicro60a64-test,repo-md,SL-Micro-6.0-Test-Pool \
+--add-repo https://susemanager.weiss.ddnss.de/ks/dist/child/staging-slmicro60a64-test-sndirsch-sidecar-slmicro60-a64/slmicro60a64-test,repo-md,sndirsch-sidecar-slmicro60-a64 \
+--add-repo https://susemanager.weiss.ddnss.de/ks/dist/child/staging-slmicro60a64-test-libnvidia-container-a64/slmicro60a64-test,repo-md,libnvidia-container-a64 \
+--add-repo https://susemanager.weiss.ddnss.de/ks/dist/child/staging-slmicro60a64-test-sl-micro-extras-6.0-pool-aarch64/slmicro60a64-test \
+--add-repo https://susemanager.weiss.ddnss.de/ks/dist/child/sl-micro-6.0-pool-aarch64-clone/slmicro60a64-test
 exit
 
-# create the bundle - required just for ova build
-#rm -rf $TARGET_DIR/image-bundle
-#mkdir -p $TARGET_DIR/image-bundle
-#kiwi-ng result bundle --target-dir $TARGET_DIR/image --bundle-dir=$TARGET_DIR/image-bundle --id=0
-
+# not required for iso building.. 
 rm -rf $TARGET_DIR/image-bundle
 mkdir -p $TARGET_DIR/image-bundle
 podman run --privileged -v $TARGET_DIR:/image:Z registry.suse.com/bci/kiwi:10.1.10 kiwi-ng result bundle --target-dir /image/image --bundle-dir=/image/image-bundle --id=0
-#!/bin/bash
-
