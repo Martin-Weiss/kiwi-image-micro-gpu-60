f955f275a688026fe5f7ffaa3d66db3c85b4f8e9
root <martin.weiss@suse.com>
2024-11-19 10:04:13 +0100

    cosmetical optimization

--- build.sh
+++ build.sh
@@ -5,6 +5,10 @@ echo "image-bundle" >>.gitignore
 
 # set variables
 TARGET_DIR=.
+#PROFILE="aarch64-self_install"
+#PROFILE="aarch64-self_install-gpu"
+PROFILE="aarch64-self_install-gpu-encrypted"
+KIWI_IMAGE="registry.suse.com/bci/kiwi:10.1.10"
 
 # clean and recreate the build folder
 rm -rf $TARGET_DIR/image
@@ -12,7 +16,7 @@ mkdir -p $TARGET_DIR/image
 
 # build the image
 
-#kiwi-ng --profile aarch64-self_install-gpu system build --target-dir $TARGET_DIR/image --description $TARGET_DIR \
+#kiwi-ng --profile $PROFILE system build --target-dir $TARGET_DIR/image --description $TARGET_DIR \
 #--add-repo https://susemanager.weiss.ddnss.de/pub/isos/slmicro60a64 \
 #--add-repo https://susemanager.weiss.ddnss.de/ks/dist/slmicro60a64-test,repo-md,SL-Micro-6.0-Test-Pool \
 #--add-repo https://susemanager.weiss.ddnss.de/ks/dist/child/staging-slmicro60a64-test-sndirsch-sidecar-slmicro60-a64/slmicro60a64-test,repo-md,sndirsch-sidecar-slmicro60-a64 \
@@ -21,8 +25,21 @@ mkdir -p $TARGET_DIR/image
 #--add-repo https://susemanager.weiss.ddnss.de/ks/dist/child/sl-micro-6.0-pool-aarch64-clone/slmicro60a64-test
 #exit
 
-podman run --privileged -v /var/lib/ca-certificates:/var/lib/ca-certificates -v /var/lib/Kiwi/repo:/var/lib/Kiwi/repo -v $TARGET_DIR/kiwi.yml:/etc/kiwi.yml  -v $TARGET_DIR:/image:Z registry.suse.com/bci/kiwi:10.1.10 kiwi-ng --debug --profile aarch64-self_install-gpu-encrypted system build --allow-existing-root --description /image --target-dir /image/image \
---ignore-repos-used-for-build --add-repo file:/var/lib/Kiwi/repo,rpm-dir,common_repo,90,false,false --add-bootstrap-package findutils --add-bootstrap-package rhn-org-trusted-ssl-cert-osimage \
+podman run --privileged \
+-v /var/lib/ca-certificates:/var/lib/ca-certificates \
+-v /var/lib/Kiwi/repo:/var/lib/Kiwi/repo \
+-v $TARGET_DIR/kiwi.yml:/etc/kiwi.yml \
+-v $TARGET_DIR:/image:Z \
+$KIWI_IMAGE kiwi-ng \
+--profile $PROFILE \
+system build \
+--allow-existing-root \
+--description /image \
+--target-dir /image/image \
+--ignore-repos-used-for-build \
+--add-repo file:/var/lib/Kiwi/repo,rpm-dir,common_repo,90,false,false \
+--add-bootstrap-package findutils \
+--add-bootstrap-package rhn-org-trusted-ssl-cert-osimage \
 --add-repo https://susemanager.weiss.ddnss.de/pub/isos/slmicro60a64 \
 --add-repo https://susemanager.weiss.ddnss.de/ks/dist/slmicro60a64-test,repo-md,SL-Micro-6.0-Test-Pool \
 --add-repo https://susemanager.weiss.ddnss.de/ks/dist/child/staging-slmicro60a64-test-sndirsch-sidecar-slmicro60-a64/slmicro60a64-test,repo-md,sndirsch-sidecar-slmicro60-a64 \
@@ -30,6 +47,7 @@ podman run --privileged -v /var/lib/ca-certificates:/var/lib/ca-certificates -v 
 --add-repo https://susemanager.weiss.ddnss.de/ks/dist/child/staging-slmicro60a64-test-sl-micro-extras-6.0-pool-aarch64/slmicro60a64-test \
 --add-repo https://susemanager.weiss.ddnss.de/ks/dist/child/sl-micro-6.0-pool-aarch64-clone/slmicro60a64-test
 exit
+--debug \
 
 # not required for iso building.. 
 rm -rf $TARGET_DIR/image-bundle
